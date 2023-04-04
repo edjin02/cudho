9b94c8a96e843593dfe68eb543d85ea2e33bbeb1
Earl <78606265+edjin02@users.noreply.github.com>
2023-04-04 13:10:17 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+    $username = $_POST['email'];
+    $password = $_POST['password'];
+
+    //Database connection
+    $con = new mysqli("localhost","root", "", "cudho");
+    if($con->connect_error){
+        die("Failed to connect : ".$con->connect_error);
+    } else {
+        $stmt = $con->prepare("select * from user where username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if($stmt_result->num_rows > 0){
+            $data = $stmt_result->fetch_assoc();
+            if($data['password'] === $password){
+                echo "<h2>Login Successfully</h2>";
+            } else {
+                echo "<h2>Invalid username or password</h2>";
+            }
+        } else {
+            echo "<h2>Invalid username or password</h2>";
+        }
+    }
+?>
